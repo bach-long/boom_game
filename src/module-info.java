dcaae61a8413079c3b722701e2a882eaf7047454
bach-long <72023201+bach-long@users.noreply.github.com>
2021-12-10 22:48:32 +0700

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module uet.oop.bomberman {
-    requires javafx.graphics;
-    requires java.desktop;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.media;
-
-    exports uet.oop.bomberman;
-}
